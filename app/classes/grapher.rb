c91b14c2f08794920f3e861e9cf6e8dacaf841b3
Mark Billie <mbillie1@gmail.com>
2014-11-19 13:16:58 -0700

    expanding graph data aggregation

--- app/classes/grapher.rb
+++ app/classes/grapher.rb
@@ -1,20 +1,36 @@
 class Grapher
+  include Months
+
   def self.total_vertical
     sum_vertical(SkiDay.all)
   end
 
   def self.available_months
-    by_month.keys
+    by_year_and_month.each_with_object({}) do |ary, years|
+      years[ary.first] = ary.last.map { |month, list| month }.sort_by { |month| MONTHS[month] }
+    end
   end
 
-  def self.monthly_vertical(month)
-    sum_vertical(by_month[month])
+  def self.monthly_vertical(month, year)
+    sum_vertical(by_year_and_month[year][month])
   end
 
   private
 
-  def self.by_month
-    SkiDay.all.group_by { |day| extract_month(day.date) }
+  def self.by_year_and_month
+    years = {}
+    by_year.each_pair do |year, list|
+      years[year] = by_month(list)
+    end
+    years
+  end
+
+  def self.by_year
+    SkiDay.all.group_by { |day| day.date.year }
+  end
+
+  def self.by_month(list)
+    list.group_by { |day| extract_month(day.date) }
   end
 
   def self.extract_month(date)
--- test/classes/grapher_test.rb
+++ test/classes/grapher_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class GrapherTest < ActiveSupport::TestCase
   describe 'aggregating graph data' do
     before { SkiDay.destroy_all }
-    after { SkiDay.destroy_all }
+    after  { SkiDay.destroy_all }
 
     describe 'vertical' do
       before do
@@ -17,18 +17,36 @@ class GrapherTest < ActiveSupport::TestCase
         end
       end
 
+      describe 'yearly' do
+        before do
+          @day_three = SkiDay.create(location: 'Brighton', date: Date.parse('13 January 2015'), vertical: 100)
+        end
+
+        it 'groups by year and then month' do
+          Grapher.available_months.must_equal({
+            2014 => ['November', 'December'],
+            2015 => ['January']
+          })
+        end
+      end
+
       describe 'monthly' do
         before do
           @day_three = SkiDay.create(location: 'Brighton', date: Date.parse('13 December 2014'), vertical: 100)
         end
 
         it 'aggregates by month' do
-          Grapher.monthly_vertical('November').must_equal 500
-          Grapher.monthly_vertical('December').must_equal 1000
+          Grapher.monthly_vertical('November', 2014).must_equal 500
+          Grapher.monthly_vertical('December', 2014).must_equal 1000
         end
 
         it 'also determines the available months' do
-          Grapher.available_months.must_equal ['November', 'December']
+          Grapher.available_months[2014].must_equal ['November', 'December']
+        end
+
+        it 'sorts months in case they are out of order in the given data' do
+          @day_four = SkiDay.create(location: 'Brighton', date: Date.parse('14 October 2014'), vertical: 1000)
+          Grapher.available_months[2014].must_equal ['October', 'November', 'December']
         end
       end
     end
